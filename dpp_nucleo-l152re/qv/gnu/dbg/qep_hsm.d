d6e7e34f5fe35313a0c32ebd7b18efc1586678b6
Aravinth Panch <me@aravinth.info>
2022-05-05 18:32:50 +0200

    Clean up dpp example

--- dpp_nucleo-l152re/qv/gnu/dbg/qep_hsm.d
+++
@@ -1,5 +0,0 @@
-dbg/qep_hsm.o: /Users/aravinth/qp/qpc/src/qf/qep_hsm.c \
- /Users/aravinth/qp/qpc/ports/arm-cm/qv/gnu/qep_port.h \
- /Users/aravinth/qp/qpc/include/qep.h \
- /Users/aravinth/qp/qpc/include/qassert.h \
- /Users/aravinth/qp/qpc/include/qs_dummy.h
